5244f80b455fbfc5df045876a53bd31599da789d
NubScript7 <ilikehacksgimme123@gmail.com>
2024-05-22 13:01:31 +0800

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -16,12 +16,26 @@ app.use(cors());
 app.use(express.json());
 app.use(asyncRouter);
 
-/* safety first! */
+/*
 app.get("/kill", (req, res) => {
   res.send("shutting down.");
   process.exit(1);
 });
-
+*/
+/*
+app.get("/testing", (req,res) => {
+  res.json({items: [{
+    title: "javascript is now in CLI! Introducing nodejs.",
+    link: "https://example.com",
+    snippet: "A new javascript experience has dropped, introducing nodejs! It uses v8 javascript engine to compile javascript codes, using the JIT or Just-In-Time compilation."
+  },
+  {
+    title: "javascript as nodejs",
+    link: "https://example.com/sub2",
+    snippet: "javascript now in nodejs! Its using the JIT or Just-In-Time compilation."
+  }]})
+})
+*/
 let messagesCount = 0;
 
 const apiKey = process.env.GOOGLE_API_KEY;
@@ -31,17 +45,28 @@ function createSearchQuery(search) {
   return `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${engineId}&q=${search}`;
 }
 
+/*
+function createSearchQuery() {
+  return `http://localhost:3000/testing`
+}
+*/
+
+let throwError = false;
+
 function send(id, msg) {
-  /*
-  spam prevention feature, when the app somehow breaks
-  and causes it to spam
-  - prevented incidents: 2
-  (increment when app breaks and spams for future devs to see
-  */
-  if (messagesCount >= 50) return;
+  
+  //spam prevention feature, when the app somehow breaks
+  //and causes it to spam
+  //- prevented incidents: 2
+  //(increment when app breaks and spams for future devs to see
+  
+  if (messagesCount >= 50)return throwError = true;
+  
+  if(throwError){
+    throw new Error("Nessage limit exceeded.")
+  }
   messagesCount += 1;
 
-  console.log("posting message: ");
   axios.post(
    `https://graph.facebook.com/v2.6/me/messages?access_token=${process.env.FB_PAGE_ACCESS_TOKEN}`,
     {
@@ -60,6 +85,7 @@ function send(id, msg) {
   })
 }
 
+/*
 app.get("/postmsg", (req,res) => {
   const id = req.query.id;
   const msg = req.query.msg;
@@ -67,16 +93,17 @@ app.get("/postmsg", (req,res) => {
   send(id,msg);
   res.sendStatus(200)
 })
+*/
 
 function scrape(msg, id) {
   const request = new Promise((resolve, reject) => {
     axios
     .get(createSearchQuery(msg))
-    .then((e) => {
+    .then(e => {
       let str = "";
       e.data.items.forEach((t, i) => {
         const { title, link, snippet } = t;
-        str += `${i + 1}.title: ${title}\n\nlink: ${link}\n\ndesc: ${snippet}\n\n\n`;
+        str += `█ ${i + 1}.title: ${title}\n\n•link: ${link}\n\n•desc: ${snippet}\n\n\n`;
       });
       return resolve(str);
     })
@@ -85,23 +112,23 @@ function scrape(msg, id) {
   
   request.then(data => {
     console.log("retrived data")
-    
-    send(id, `typeof data: ${typeof data}`)
-    send(id, `FETCHED: \n\n${data}`)
+    send(id, data)
   })
   .catch(err => {
-    console.log("error",err.messsage,err.request)
+   console.log(err)
     send(id, "Sorry!, i couldn't process your message, please try again later.")
   });
 }
+
 
 
 app.post("/webhook", (req, res) => {
   if (req.body.object === "page") {
     for (const entry of req.body.entry) {
       const [user] = entry.messaging;
+      console.log(user)
       const senderId = user.sender.id;
-      const msg = user.message?.text;
+      const msg = user.message.text;
       if (!msg)return console.log("message was empty.");
       
       scrape(msg, senderId);
